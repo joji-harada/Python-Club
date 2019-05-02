5f83919205b835ddc413d39a4c0097bf7e01f4f6
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-05-01 21:27:38 -0700

    meetingDetail commit

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 #takes proj urls include and transfers it to the views render request
 urlpatterns=[
     path('', views.index, name='index'),
-    path('getResource', views.getResource, name='resource')
+    path('getResource', views.getResource, name='resource'),
+    path('getMeeting', views.getMeeting, name='meeting'),
+    path('meetingDetail/<int:id>', views.meetingDetail, name='meetingdetail'),
 ]
--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinute, Resource, Event
 
 
@@ -10,4 +10,17 @@ def index(request):
 def getResource(request):
     resource_list = Resource.objects.all()
     context = {'resource_list' : resource_list}
-    return render(request, 'clubapp/resource.html', context=context)
+    return render(request, 'clubapp/resource.html', context = context)
+
+def getMeeting(request):
+    meeting_list = Meeting.objects.all()
+    return render(request, 'clubapp/meeting.html', context = {'meeting_list' : meeting_list})
+
+def meetingDetail(request, id):
+    meet = get_object_or_404(Meeting, pk=id)
+    location = Meeting.location
+    context = {
+        'meet' : meet,
+        'location' : location,
+    }
+    return render(request, 'clubapp/meetingdetail.html', context = context)
