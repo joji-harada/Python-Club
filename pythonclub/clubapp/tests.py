cb5a2ca812d8fe4623dd9dd7ae0562f4d0367ccd
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-05-08 16:29:09 -0700

    Test Commit

--- pythonclub/clubapp/tests.py
+++ pythonclub/clubapp/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Meeting, MeetingMinute, Resource
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class MeetingTest(TestCase):
+    def test_string(self):
+        type = Meeting(titleid='1')
+        self.assertEqual(str(type), type.titleid)
+
+    def test_table(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+
+class MeetingMinuteTest(TestCase):
+    def test_string(self):
+        type = MeetingMinute(durationid='60')
+        self.assertEqual(str(type), type.durationid)
+
+    def test_table(self):
+        self.assertEqual(str(MeetingMinute._meta.db_table), 'meetingminute')
+
+
+class ResourceTest(TestCase):
+    def test_string(self):
+        type = Resource(resourceid='W3')
+        self.assertEqual(str(type), type.resourceid)
+
+    def test_table(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
